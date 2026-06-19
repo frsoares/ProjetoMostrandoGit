79b6a02aa523bd1775d06ba5ae4a72471993e455
Francisco Soares <fmssn@cin.ufpe.br>
2026-06-19 17:24:26 -0300

    besteira!

--- ProjetoMostrandoGit/ContentView.swift
+++ ProjetoMostrandoGit/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  ProjetoMostrandoGit
 //
-//  Created by Francisco Miranda Soares on 19/06/26.
+//  Created by Francisco Miranda Soares on 19/06/26...
 //
 
 import SwiftUI
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, world?!")
         }
         .padding()
     }
