3df0145db4d40beac083de32f74ef02269a6f661
Francisco Soares <fmssn@cin.ufpe.br>
2026-06-19 17:06:15 -0300

    Initial Commit

---
+++ ProjetoMostrandoGit/ProjetoMostrandoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjetoMostrandoGitApp.swift
+//  ProjetoMostrandoGit
+//
+//  Created by Francisco Miranda Soares on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjetoMostrandoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
